95c2e5d0d0fdbad56d777e7c40bf07c618555941
LeenSamZiv <1179364851@qq.com>
2019-06-12 10:23:34 +0800

    ✔ close vue-dev-tool with production environment

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import {Util} from "./utils/util";
 
+//关闭vue-dev-tool
+const isDebug_mode = process.env.NODE_ENV !== 'production';
+Vue.config.debug = isDebug_mode;
+Vue.config.devtools = isDebug_mode;
+Vue.config.productionTip = isDebug_mode;
+
 Vue.config.productionTip = false;
 Vue.prototype.$util = Util;
 new Vue({
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    publicPath: "./",
+    productionSourceMap: process.env.NODE_ENV === "development",
+};
